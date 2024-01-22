0a92fb9252ebd8a18b6b292f8667c05382af87fa
ShohruhEgamov <shohruhwebdev@gmail.com>
2024-01-22 10:23:24 +0300

    muammo hal qilisin

--- shoh.py
+++ shoh.py
@@ -7,9 +7,11 @@ print(c)
 print("Salom Dunyo")
 print("yana ozgarish")
 print("mahallidan qoshaman")
-
+<<<<<<< kuzatish
 class MyClass:
   x = 5
 
 p1 = MyClass()
 print(p1.x)
+=======
+>>>>>>> master
