0d58f6e89c7991f26d1ce97c2e2905572b081a47
ShohruhEgamov <shohruhwebdev@gmail.com>
2024-01-22 11:39:11 +0300

    yana json

--- shoh.py
+++ shoh.py
@@ -35,3 +35,19 @@ y = json.loads(x)
 
 # the result is a Python dictionary:
 print(y["age"])
+
+
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
