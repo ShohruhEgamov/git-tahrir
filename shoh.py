0d961672677c857af9b9fce3e07f14463c21f593
ShohruhEgamov <135800314+ShohruhEgamov@users.noreply.github.com>
2024-01-22 11:32:59 +0300

    Update shoh.py

--- shoh.py
+++ shoh.py
@@ -24,3 +24,14 @@ p1 = Person("John", 36)
 
 print(p1.name)
 print(p1.age)
+
+import json
+
+# some JSON:
+x = '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
