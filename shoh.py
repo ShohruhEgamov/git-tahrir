d08a5473602fc19da173ab5d41f8ddecfda22a29
ShohruhEgamov <shohruhwebdev@gmail.com>
2024-01-22 10:50:22 +0300

    yangi clash +

--- shoh.py
+++ shoh.py
@@ -15,3 +15,12 @@ class MyClass:
 p1 = MyClass()
 print(p1.x)
 
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
